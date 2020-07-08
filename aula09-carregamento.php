df469e1877dcda38e2f5fe7cd97764fd79d15606
Luiz Antonio Andrade de Sousa <luizdsousa@gmail.com>
2020-07-08 12:13:14 -0300

    Aula de autoload

--- aula09-carregamento.php
+++ aula09-carregamento.php
@@ -1,9 +1,24 @@
 <?php
 echo "<h2>#AUTOLOAD SPL PSR-4</h2>";
 
+//require __DIR__ . "/source/Loading/User.php";
+//require __DIR__ . "/source/Loading/Address.php";
+//require __DIR__ . "/source/Loading/Company.php";
 
+require __DIR__ . "/source/autoload.php";
+
+$user = new \Source\Loading\User();
+$address = new \Source\Loading\Address();
+$company = new \Source\Loading\Company();
+
+echo "<pre>", var_dump(
+    $user,
+    $address,
+    $company
+), "</pre>";
 
 echo "<h2>#AUTOLOAD COMPOSER PSR-4</h2>";
 
 
-?>
+
+?> 
